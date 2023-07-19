e5ca0f1e33937b3c6f266f6149634eb3e4557936
longestcow <omiyer76@gmail.com>
2023-07-19 07:48:25 -0600

    main commit

---
+++ main.js
@@ -0,0 +1,70 @@
+let images = null;
+let maxX = document.body.scrollWidth;
+let curVal = 3;
+
+function moveHead(event) {
+    if(images==null){
+        return;
+    }
+    let x = event.clientX;
+    let val = Math.round(map(x, 0, maxX, 0, 6));
+
+    if(val!=curVal){
+        images[curVal].style.visibility='hidden';
+        curVal=val;
+        images[curVal].style.visibility='visible';
+    }
+
+}
+
+function map(input, input_start, input_end, output_start, output_end){
+    return output_start + ((output_end - output_start) / (input_end - input_start)) * (input - input_start);
+}
+
+function resized() {
+    maxX = document.body.scrollWidth;
+    console.log(maxX);
+}
+
+function hovered(st) {
+    document.getElementById(st).classList.add("hovering");
+}
+function unhovered(st) {
+    document.getElementById(st).classList.remove("hovering");
+}
+function clicked(i){
+    document.getElementById('transELM').style.backgroundColor=((i==1)?'#FE4773':(i==2)?'#F6D68D':'#46B3A5');
+}
+
+function transLoaded(){
+    document.getElementById('transELM').style.backgroundColor=localStorage.getItem("prevColor");
+    console.log("aaaaaaaaaaaaaaaa")
+}
+
+window.onload = () => {
+    const transitionELM = document.querySelector('.transition');
+    let pathName = window.location.pathname;
+
+    localStorage.setItem("prevColor", (pathName.includes("contact"))?"#46B3A5":(pathName.includes("projects"))?"#FE4773":(pathName.includes("about"))?"#F6D68D":localStorage.getItem("prevColor"))
+    if(pathName.includes("projects")){
+        addProjects();
+    }
+    transitionELM.classList.remove('is-active');
+    images = [document.getElementById("head1"),document.getElementById("head2"),document.getElementById("head3"),document.getElementById("head4"),document.getElementById("head5"),document.getElementById("head6"),document.getElementById("head7")];
+    const anchors = document.querySelectorAll('a');
+    for(let i = 0; i<anchors.length; i++) {
+        let anchor = anchors[i];
+        if(!(anchor.href.includes("index") || anchor.href.includes("projects") || anchor.href.includes("about") || anchor.href.includes("contact"))){
+            continue;
+        }
+        anchor.addEventListener('click', e => {
+            e.preventDefault();
+            let target = anchor.href;
+            transitionELM.classList.add('is-active');
+            setTimeout(() => {
+              window.location.href = target;
+            }, 400);
+          })
+    }
+
+}
---
+++ projectsLoad.js
@@ -0,0 +1,99 @@
+const projectsData = [
+    {
+        "name":"this site",
+        "year":2023,
+        "status":"finished",
+        "desc":"literally this site",
+        "lang":["html","js","css"],
+        "links":["https://github.com/longestcow/portfolio"]
+    },
+    {
+        "name":"titato",
+        "year":2023,
+        "status":"finished",
+        "desc":"its tictactoe but uh you can't win",
+        "lang":["java","unity"],
+        "links":["https://github.com/longestcow/titato"]
+    },
+    {
+        "name":"platformer",
+        "year":2022,
+        "status":"unfinished",
+        "desc":"its a platformer",
+        "lang":["unity"],
+        "links":["https://github.com/longestcow/platformer"]
+    },
+    {
+        "name":"platform fighter",
+        "year":2023,
+        "status":"unfinished",
+        "desc":"i made this for a school project",
+        "lang":["unity"],
+        "links":["https://github.com/longestcow/platformFighter"]
+    },
+    {
+        "name":"mirror reflection",
+        "year":2023,
+        "status":"finished",
+        "desc":"light mirror collision simulation",
+        "lang":["java"],
+        "links":["https://github.com/longestcow/MirrorReflection"]
+    },
+    {
+        "name":"omogodot's GOL",
+        "year":2022,
+        "status":"finished",
+        "desc":"cellular automata",
+        "lang":["java"],
+        "links":["https://github.com/longestcow/GameOfLife"]
+    }
+]
+
+addProjects = function(){
+    var container = document.getElementById("cardsHolder");
+    for(var i = 0; i<projectsData.length; i++){
+        let projInfo = projectsData[i];
+        var mainCard = document.createElement("div");
+        mainCard.className='card mainCard';
+        var cardBody = document.createElement("div");
+        cardBody.className='card-body';
+        mainCard.appendChild(cardBody);
+
+        var title = createELM("p", "card-title cardComp", "margin-bottom:15px", "");
+        var titleLink = createELM("a", "pLink", "", projInfo["name"]);
+        titleLink.href=projInfo["links"][0];
+        titleLink.target="_blank";
+        title.appendChild(titleLink);
+        cardBody.appendChild(title);
+
+        var yearStatus = createELM("p", "card-subtitle mb-2 text-body-secondary cardComp", "font-size:0.85em;margin-bottom:5px", projInfo["year"]+" - "+projInfo["status"])
+        cardBody.appendChild(yearStatus);
+
+        var desc = createELM("p", "card-text cardComp", "font-size:0.8em;color:#999999;margin-bottom:8px", projInfo["desc"])
+        cardBody.appendChild(desc);
+        var curText="";
+        for(var j = 0; j<projInfo["lang"].length; j++){
+            if(j!=0){
+                curText+="<span style='color:white'>, </span>";
+            }
+            curText+=projInfo["lang"][j];
+        }
+
+        var langs = createELM("p", "card-text cardComp", "font-size:0.8em;color:#FE4773;margin-bottom:5px", curText);
+
+        cardBody.appendChild(langs);
+
+        container.appendChild(mainCard);
+        console.log(projInfo["name"]+": "+projInfo["year"]+" - "+projInfo["status"]+", '"+projInfo["desc"]+"', "+projInfo["lang"]+", "+projInfo["links"]);
+
+    }
+}
+
+function createELM(type, classN, style, inner){
+    var newELM = document.createElement(type);
+    newELM.className=classN;
+    newELM.style=style;
+    newELM.innerHTML=inner;
+
+    return newELM;
+}
